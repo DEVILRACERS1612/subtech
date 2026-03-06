0b492b7026f2921a16ecfca00e45e98d733aba47
Microelectra <business@microelectra.in>
2026-03-06 13:52:33 +0530

    Delete techapi directory

--- techapi/helpers/response.php
+++
@@ -1,9 +0,0 @@
-<?php
-function response($status, $message, $data = []) {
-    echo json_encode([
-        "status" => $status,
-        "message" => $message,
-        "data" => $data
-    ]);
-    exit;
-}
